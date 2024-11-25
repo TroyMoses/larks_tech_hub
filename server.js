fd36f9f6fd2efef20c7705ecfb533ff07b027d64
TroyMoses <mugabimoses07@gmail.com>
2024-11-25 15:08:12 +0300

    email now can send well

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+
+const port = process.env.PORT;
+app.listen(5000, () => console.log("Server is Running on port " + port));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
@@ -27,20 +27,21 @@ contactEmail.verify((error) => {
   if (error) {
     console.log(error);
   } else {
-    console.log("Ready to Send");
+    console.log("Ready to Send Contact Emails.");
   }
 });
 
 router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
   const phone = req.body.phone;
   const mail = {
-    from: name,
+    from: firstName,
     to: process.env.EMAIL_USER,
     subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
+    html: `<p>Name: ${firstName} ${lastName}</p>
            <p>Email: ${email}</p>
            <p>Phone: ${phone}</p>
            <p>Message: ${message}</p>`,
